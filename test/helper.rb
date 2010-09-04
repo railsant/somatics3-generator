dfab9c0cf10f1d14e7806f48e2cd8caa10d15f15
tkwong <tkwong@inspiresynergy.com>
2010-09-04 13:57:26 +0800

    Initial commit to somatics3-generators.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'somatics3-generators'
+
+class Test::Unit::TestCase
+end
